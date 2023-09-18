786b1cecc9e4eb60f36d562a8d4eff2924c190cb
Tayte Boss <boss.tayte@gmail.com>
2023-09-18 14:24:02 +0700

    [feat] 404

--- frontend/pages/404.tsx
+++ frontend/pages/404.tsx
@@ -1,15 +1,39 @@
 import { NextSeo } from 'next-seo';
 import styled from 'styled-components';
+import LayoutWrapper from '../components/common/LayoutWrapper';
+import Link from 'next/link';
+import pxToRem from '../utils/pxToRem';
 
-const PageWrapper = styled.div``;
+const PageWrapper = styled.div`
+	position: relative;
+	z-index: 15;
+	pointer-events: all;
+`;
+
+const Inner = styled.div`
+	padding-top: ${pxToRem(40)} 0;
+`;
+
+const Title = styled.h1`
+	margin-bottom: ${pxToRem(24)};
+`;
+
+const HomeLink = styled.a``;
 
 const Page = () => {
 	return (
 		<PageWrapper>
 			<NextSeo
-				title="Boiler"
-				description="Boiler Plate"
+				title="One Hope St - 404"
 			/>
+			<LayoutWrapper>
+				<Inner>
+					<Title>We couldn't find that page</Title>
+					<Link href="/" passHref>
+						<HomeLink>Go back home</HomeLink>
+					</Link>
+				</Inner>
+			</LayoutWrapper>
 		</PageWrapper>
 	)
 }
